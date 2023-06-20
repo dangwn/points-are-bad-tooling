b1da457284970b7069e65784ae10362fccee6b4e
dangwn <dangawne@outlook.com>
2023-06-20 16:35:57 +0100

    logger

---
+++ pablogger/logger.go
@@ -0,0 +1,36 @@
+package pablogger
+
+import (
+	"log"
+	"os"
+)
+
+type PABLogger struct {
+	DebugLogger *log.Logger
+	InfoLogger *log.Logger
+	WarningLogger *log.Logger
+}
+
+func customLoggerComponent(level string) *log.Logger {
+	return log.New(os.Stdout, " [" + level + "] ", log.Ldate | log.Ltime)
+}
+
+func NewLogger() *PABLogger {
+	return &PABLogger{
+		DebugLogger: customLoggerComponent("DEBUG"),
+		InfoLogger: customLoggerComponent("INFO"),
+		WarningLogger: customLoggerComponent("WARNING"),
+	}
+}
+
+func (p *PABLogger) Debug(msg any) {
+	p.DebugLogger.Println(msg)
+}
+
+func (p *PABLogger) Info(msg any) {
+	p.InfoLogger.Println(msg)
+}
+
+func (p *PABLogger) Warning(msg any) {
+	p.WarningLogger.Println(msg)
+}
